b4f77a9e0b378752133c13c22952534049784709
Khaidir096 <40510753+Khaidir096@users.noreply.github.com>
2019-07-31 14:20:20 +0000

    Add files via upload

---
+++ trial.php
@@ -0,0 +1,13 @@
+<?php
+// Warna Terminal
+$biru = "\e[34m";
+$kuning = "\e[33m";                                             $cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$m = "\e[91m";
+$b = " \033[1m";
+
+echo " $cyan$b PROXY GENERATOR (FEEE VERSION ^_^):D \n >";
+echo " $m INFO! ";
+echo file_get_contents("https://pastebin.com/raw/2yMvcPzL");
+?>
